ba1013bad0cae7840c184b17af3280db198df98d
VivekD-123 <dongarevivek81@gmail.com>
2025-07-09 20:41:01 +0530

    feat: initial working local build

---
+++ data/global.ts
@@ -0,0 +1,105 @@
+type Route = {
+  title: string,
+  path: string
+}
+
+type FooterCol = {
+  title: string,
+  links: {
+    name: string,
+    link: string,
+    icon?: string,
+    leavesWebsite: boolean
+  }[]
+}
+
+type Footer = {
+  columns: FooterCol[]
+  support: {
+    buymeacoffee: string
+    message: string
+  }
+};
+
+export const routes: Route[] = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  // {
+  //   title: "Blog",
+  //   path: "/blog",
+  // },
+  {
+    title: "Projects",
+    path: "/projects",
+  },
+  // {
+  //   title: "Designs",
+  //   path: "/designs",
+  // },
+];
+
+
+export const footer: Footer = {
+  columns: [
+    {
+      title: "Pages",
+      links: [
+        {
+          name: "Home",
+          link: "/",
+          leavesWebsite: false,
+        },
+        // {
+        //   name: "Blog",
+        //   link: "/blog",
+        //   leavesWebsite: false,
+        // },
+        {
+          name: "Projects",
+          link: "/projects",
+          leavesWebsite: false,
+        },
+        // {
+        //   name: "Designs",
+        //   link: "/designs",
+        //   leavesWebsite: false,
+        // },
+      ],
+    },
+    {
+      title: "Social",
+      links: [
+        {
+          name: "Email",
+          link: "mailto:dongarevivek81@gmial.com",
+          icon: "/static/icons/gmail.svg",
+          leavesWebsite: true,
+        },
+        {
+          name: "GitHub",
+          link: "https://github.com/vivekdongare81",
+          icon: "/static/icons/github.svg",
+          leavesWebsite: true,
+        },
+        {
+          name: "LinkedIn",
+          link: "https://www.linkedin.com/in/vivek-dongare-50823a18b/",
+          icon: "/static/icons/linkedin.svg",
+          leavesWebsite: true,
+        },
+        {
+          name: "LeetCode",
+          link: "https://leetcode.com/u/dongarevivek81/",
+          icon: "/static/icons/leetcode.svg",
+          leavesWebsite: true,
+        },
+      ],
+    },
+  ],
+  support: {
+    buymeacoffee: "dongarevivw",
+    message: "I appreciate your support very much! 💙",
+  },
+};
---
+++ pages/index.tsx
@@ -0,0 +1,37 @@
+import CTA from "@/components/home/CTA";
+import Hero from "@/components/home/Hero";
+import Page from "@/components/utility/Page";
+// import Posts from "@/components/home/Posts";
+import Projects from "@/components/home/Projects";
+import Skills from "@/components/home/Skills";
+import Experiences from "@/components/home/Experience";
+// import { allPosts } from "contentlayer/generated";
+// import { GetStaticProps } from "next";
+
+// export const getStaticProps: GetStaticProps = async () => {
+//   return {
+//     props: {
+//       allPosts: allPosts.sort(({date: dateA}: any, {date: dateB}: any) => dateB - dateA),
+//     },
+//   };
+// }
+
+export default function Home() {
+  return (
+    <Page
+      currentPage="Home"
+      meta={{
+        desc: "I'm a passionate web developer and designer coding beautiful websites and apps.",
+      }}
+    >
+      <Hero />
+      <div className="mt-20 space-y-32">
+        <Experiences />
+        <Skills />
+        <Projects />
+        {/* <Posts allPosts={allPosts} /> */}
+      </div>
+      <CTA />
+    </Page>
+  );
+}
