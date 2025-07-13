1217d76597a85abf92d07fb423b1c80593e58b77
VivekD-123 <dongarevivek81@gmail.com>
2025-07-14 01:17:58 +0530

    feat: add projects data

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -10,38 +10,42 @@ function ProjectCard({ project }) {
       key={project.id}
     >
       <a
-        href={project.link || project.github}
+        href={project.github}
         target="_blank"
+        rel="noreferrer"
         className={`w-full relative rounded-xl border-fun-gray border p-2 transition hover:-translate-y-2 hover:opacity-75 hover:border-fun-pink will-change-projectCard`}
       >
         <img
           className="w-full rounded-md"
           src={project.img}
+          alt={project.title}
         />
       </a>
       <div className="w-full mt-5">
         <div className="flex projects-center justify-between">
-          <a href={project.link || project.github} target="_blank">
+          <a href={project.github} target="_blank" rel="noreferrer">
             <h3 className="text-lg font-bold">{project.title}</h3>
           </a>
           <div className="space-x-2">
             {project.link && (
-              <a href={project.link} target="_blank" rel="noreferrer">
+              <a href={project.link} target="_blank" rel="noreferrer" className="hover:opacity-75">
                 <Image
                   src="/static/icons/external-link.svg"
                   width={16}
                   height={16}
-                  alt="Link Icon"
+                  alt="Visit Website"
+                  title="Visit Website"
                 />
               </a>
             )}
             {project.github && (
-              <a href={project.github} target="_blank" rel="noreferrer">
+              <a href={project.github} target="_blank" rel="noreferrer" className="hover:opacity-75">
                 <Image
                   src="/static/icons/github.svg"
                   width={16}
                   height={16}
-                  alt="Github Icon"
+                  alt="View on GitHub"
+                  title="View on GitHub"
                 />
               </a>
             )}
